c40941675e0ed6a684b34b4f723e99e3c37fcc3f
shephali09 <hiremathshephali9@gmail.com>
2023-03-18 10:29:55 +0530

    Add: Snake and ladder game played with single player at start position 0

---
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderSimulator.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.snakeandladderproblem;
+
+/* Snake and Ladder game played with
+single player at start position 0 */
+
+public class SnakeAndLadderSimulator {
+
+	public static void main(String[] args) {
+		
+		int startPosition = 0;
+
+	}
+
+}
