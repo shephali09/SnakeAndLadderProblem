92b406798f34a78dadd90751b863f1294f457277
shephali09 <hiremathshephali9@gmail.com>
2023-03-19 14:03:50 +0530

    Add: Ensure the player gets exact winning position 100. In case the player postion go above 100, player stays in the same previous position till the player gets the exact

--- src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadderSimulator.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.snakeandladderproblem;
 
-/* Repeat till the Player reaches the winning position 100. 
- * - Note In case the player position moves below 0, then the player restarts from 0 */
+/* Ensure the player gets to exact winning position 100.
+ * In case the player position go above 100, the player stays 
+ * in the same previous position till the player gets the exact */
 
 public class SnakeAndLadderSimulator {
 	
@@ -13,6 +14,7 @@ public class SnakeAndLadderSimulator {
 		
 		currentPosition = startPosition;
 		while(currentPosition < finalPosition) {
+			
 			//Generates value between 1 to 6 for die
 			int diceValue = (int) (Math.floor(Math.random() * 10) % 6) + 1;
 			System.out.println("The player get dice value as: " + diceValue);
@@ -23,13 +25,17 @@ public class SnakeAndLadderSimulator {
         
 			/*In Case of No Play the player stays in the same position
 			 * - In Case of Ladder the player moves ahead by the number of position received in the die
-			 * - In Case of Snake the player moves behind by the */
+			 * - In Case of Snake the player moves behind by the
+			 * - In case the player position go above 100, the player stays in the same previous position till the player gets the exact */
 			if(option == 0) {
 				System.out.println("No play");
 				currentPosition += 0;
 			} else if(option == 1) {
 				System.out.println("Ladder");
 				currentPosition += diceValue;
+				if(currentPosition > 100 ) {
+					currentPosition -= diceValue;
+				}
 			} else {
 				System.out.println("Snake");
 				currentPosition -= diceValue;
